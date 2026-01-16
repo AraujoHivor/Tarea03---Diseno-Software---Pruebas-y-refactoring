0442d1c2de950ca65b151d608f4f28b2b3c59c5a
XavierLopezM <xlopezm@espol.edu.ec>
2026-01-15 22:48:45 -0500

    NotificadorStrategy

--- Tarea 03 -Diseño Sotware/src/com/Strategy/NotificadorStrategy.java
+++ Tarea 03 -Diseño Sotware/src/com/Strategy/NotificadorStrategy.java
@@ -1,5 +1,5 @@
 package com.Strategy;
 
 public interface NotificadorStrategy {
-    void notificar(String destinatario, String asunto, String mensaje);
+    void notificar(ContenidoNotificacion contenido);
 }
