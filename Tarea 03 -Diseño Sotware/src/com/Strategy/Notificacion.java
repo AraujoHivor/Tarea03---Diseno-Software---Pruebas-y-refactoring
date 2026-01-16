932ab2d01502677584580f2f99278f1d59184c16
XavierLopezM <xlopezm@espol.edu.ec>
2026-01-15 22:47:58 -0500

    Refactorizando Strategy

--- Tarea 03 -Diseño Sotware/src/com/Strategy/Notificacion.java
+++ Tarea 03 -Diseño Sotware/src/com/Strategy/Notificacion.java
@@ -2,27 +2,21 @@ package com.Strategy;
 
 public class Notificacion {
 
-    private String destinatario;
-    private String asunto;
-    private String mensaje;
+    private ContenidoNotificacion contenido;
     private NotificadorStrategy strategy;
 
-    public Notificacion(String destinatario, String asunto, String mensaje) {
-        this.destinatario = destinatario;
-        this.asunto = asunto;
-        this.mensaje = mensaje;
+    public Notificacion(ContenidoNotificacion contenido) {
+        this.contenido = contenido;
     }
 
     public void setStrategy(NotificadorStrategy strategy) {
         this.strategy = strategy;
     }
 
-    public void notificar(String destinatario, String asunto, String mensaje) {
-        
+    public void notificar() {
         if (strategy == null) {
             throw new IllegalStateException("No se ha definido una estrategia de notificación");
         }
-        strategy.notificar(destinatario, asunto, mensaje);
+        strategy.notificar(contenido);
     }
-
 }
